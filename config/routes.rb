37fda96b73b75954b2dd55bac52abcd57f97bfb5
Brad Edwards <bledwards1@gmail.com>
2017-08-12 17:16:05 -0500

    tutorial updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 root 'demo#index'
 
   get 'demo/index'
+  get 'demo/hello'
   # may go away in future versions of rails
   #get ':controller(/:action(/:id))'
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
