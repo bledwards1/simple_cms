edd3a53c9c0ca4f45d91ac7e4081f872097b01bf
Brad Edwards <bledwards1@gmail.com>
2017-08-18 18:04:37 -0500

    Tutorial progress - Chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ root 'demo#index'
 
   get 'demo/index'
   get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
+
   # may go away in future versions of rails
   #get ':controller(/:action(/:id))'
 
